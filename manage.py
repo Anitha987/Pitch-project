d69ef36ca932c7a6930d8716d79790e4ce228b2f
BlandineK <blandineuwase222@gmail.com>
2019-09-20 13:03:11 +0200

    add role class

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 from app import create_app,db
 from app.models import User 
 
+app = create_app('development')
+
+manager = Manager(app)
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
