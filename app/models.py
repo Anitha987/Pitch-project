d09d82cc27a7b35decea19835e3c9bedb51c8e5c
BlandineK <blandineuwase222@gmail.com>
2019-09-20 15:12:28 +0200

    add classes in models file

--- app/models.py
+++ app/models.py
@@ -3,15 +3,34 @@ class User(db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(255))
+  pitch_id=db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
   def __repr__(self):
-      return f'User {self.username}'
+    return f'User {self.username}'
 
-class Role(db.Model):
-  __tablename__ = 'roles'
+class Pitch(db.Model):
+  __tablename__ = 'pitches'
 
   id = db.Column(db.Integer,primary_key = True)
   name = db.Column(db.String(255))
+  # users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+
 
   def __repr__(self):
-      return f'User {self.name}'
+    return f'User {self.name}'
+
+class Category(db.Model): 
+  __tablename__= 'categories' 
+  id = db.Column(db.Integer,primary_key=True)
+  name = db.Column(db.String(255))
+
+class Comment(db.Model):
+  __tablename__='comments'
+  id = db.Column(db.Integer,primary_key=True)
+  name = db.Column(db.String(225))
+
+class Vote(db.Model):
+  __tablename__='votes'
+  id = db.Column(db.Integer,primary_key=True)
+  name = db.Column(db.String(225))
+
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User 
+from app.models import User,Pitch,Category,Comment,Vote
 
 app = create_app('development')
 
@@ -7,6 +7,6 @@ manager = Manager(app)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment,Vote = Vote )
 if __name__ == '__main__':
     manager.run()
