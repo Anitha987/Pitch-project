f415ca794d7a496f1a4f76ba82099cab169a32b3
BlandineK <blandineuwase222@gmail.com>
2019-09-20 16:15:08 +0200

    add class of comment in models.py file

--- app/models.py
+++ app/models.py
@@ -6,7 +6,17 @@ class User(db.Model):
   username = db.Column(db.String(255))
   pass_secure = db.Column(db.String(225))
   pitch_id=db.Column(db.Integer,db.ForeignKey('pitches.id'))
+  @property
+  def password(self):
+      raise AttributeError('You cannot read the password attribute')
 
+  @password.setter
+  def password(self, password):
+      self.pass_secure = generate_password_hash(password)
+
+
+  def verify_password(self,password):
+      return check_password_hash(self.pass_secure,password)
   def __repr__(self):
     return f'User {self.username}'
 
@@ -15,7 +25,7 @@ class Pitch(db.Model):
 
   id = db.Column(db.Integer,primary_key = True)
   name = db.Column(db.String(255))
-  # users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+  users = db.relationship('User',backref = 'pitch',lazy="dynamic")
 
 
   def __repr__(self):
